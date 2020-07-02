323f2ce0b231f7708d6cea10def2970a80750d40
Adam Tiller <a.j.tiller@gmail.com>
2020-07-02 08:48:45 -0700

    02 Recursive

--- JulyChallenge/02.BinaryTreeLevelOrderTraversalII.js
+++ JulyChallenge/02.BinaryTreeLevelOrderTraversalII.js
@@ -11,35 +11,23 @@
  * @return {number[][]}
  */
 var levelOrderBottom = function(root) {
-  if (root === null) {
-    return [];
-  }
-
-  const queue = [{depth: 0, node: root}];
   const result = [];
-  let currentLevel = [];
-  let currentDepth = 0;
 
-  while (queue.length > 0) {
-    const current = queue.shift();
-
-    if (current.depth > currentDepth) {
-      result.unshift(currentLevel);
-      currentLevel = [];
-      currentDepth++;
+  const breadthFirst = (node, depth) => {
+    if (node === null) {
+      return;
     }
 
-    currentLevel.push(current.node.val);
+    if (result[depth] === undefined) {
+      result[depth] = [];
+    }
 
-    if (current.node.left !== null) {
-      queue.push({depth: currentDepth + 1, node: current.node.left});
-    }
-    if (current.node.right !== null) {
-      queue.push({depth: currentDepth + 1, node: current.node.right});
-    }
+    result[depth].push(node.val);
+    breadthFirst(node.left, depth + 1);
+    breadthFirst(node.right, depth + 1);
   }
 
-  result.unshift(currentLevel);
+  breadthFirst(root, 0);
 
-  return result;
+  return result.reverse();
 };
