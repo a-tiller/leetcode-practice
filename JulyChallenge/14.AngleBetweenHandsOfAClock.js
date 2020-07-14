33fff36c07c20fe689bf8f35492cad8dfbf3ee0b
Adam Tiller <a.j.tiller@gmail.com>
2020-07-14 09:44:41 -0700

    Just do the math.

---
+++ JulyChallenge/14.AngleBetweenHandsOfAClock.js
@@ -0,0 +1,12 @@
+var angleClock = function(hour, minutes) {
+
+  const minuteAngle = minutes * 6;
+
+  const hourAngle = ((hour % 12) * 30) + ((minutes / 60) * 30);
+
+  const lower = Math.min(minuteAngle, hourAngle);
+
+  const higher = Math.max(minuteAngle, hourAngle);
+
+  return Math.min(higher - lower, lower + 360 - higher);
+};
