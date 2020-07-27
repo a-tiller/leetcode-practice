b0d81820179b9c2f0fdb1e6b982cb70197ad0e5d
Adam Tiller <a.j.tiller@gmail.com>
2020-07-27 06:07:42 -0700

    Pointer method.

--- JulyChallenge/27.ConstructBinaryTreeFromInorderAndPostorderTraversal.js
+++ JulyChallenge/27.ConstructBinaryTreeFromInorderAndPostorderTraversal.js
@@ -1,18 +1,29 @@
 var buildTree = function(inorder, postorder) {
 
-  if (inorder.length === 0) return null;
+  if (!inorder.length) return null;
 
-  let node = new TreeNode(postorder[postorder.length - 1])
-  let pos = inorder.indexOf(node.val);
+  let poPos = postorder.length - 1;
 
+  const valueToIndex = new Map();
+  inorder.forEach((v, i) => {
+    valueToIndex.set(v, i);
+  });
 
-  if (pos > 0) {
-    node.left = buildTree(inorder.slice(0, pos),  postorder.slice(0, pos));
+  const treeBuilder = (start, end) => {
+
+    if (start > end) return null;
+
+    const node = new TreeNode(postorder[poPos]);
+    poPos--;
+
+    if (start === end) return node;
+
+    const ioPos = valueToIndex.get(node.val);
+    node.right = treeBuilder(ioPos + 1, end);
+    node.left = treeBuilder(start, ioPos - 1);
+
+    return node;
   }
 
-  if (pos < inorder.length - 1) {
-    node.right = buildTree(inorder.slice(pos + 1), postorder.slice(pos, postorder.length - 1));
-  }
-
-  return node;
+  return treeBuilder(0, inorder.length - 1);
 };
