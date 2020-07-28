5d6caf250189da6a7709349fd9a75d5791030a1e
Adam Tiller <a.j.tiller@gmail.com>
2020-07-28 12:36:24 -0700

    Queue management.

--- SlidingWindowMaximum/SlidingWindowMaximum.js
+++ SlidingWindowMaximum/SlidingWindowMaximum.js
@@ -1,16 +1,35 @@
 var maxSlidingWindow = function(nums, k) {
-  let result = nums.slice(0, nums.length - k + 1);
+  const queue = [];
 
-  for (let i = 1; i < k; i++) {
-    result = result.map((value, index) => {
-      return Math.max(value, nums[index + i])
-    })
+  const updateQ = (value, index) => {
+    if (queue.length !== 0 && queue[0] === index - k) {
+      queue.shift();
+    }
+
+    while (queue.length !== 0 && value >= nums[queue[queue.length - 1]]) {
+      queue.pop();
+    }
+
+    queue.push(index);
+  }
+
+  for (let i = 0; i < k; i++ ) {
+    const num = nums[i]
+    updateQ(num, i)
+  }
+
+  const result = [nums[queue[0]]];
+
+  for (let i = k; i < nums.length; i++) {
+    const val = nums[i];
+    updateQ(val, i);
+    result.push(nums[queue[0]]);
   }
 
   return result;
 };
 
-let nums = [1,3,-1,-3,5,3,6,7];
-let k = 3;
+// let nums = [9,10,9,-7,-4,-8,2,-6];
+// let k = 5;
 
-console.log(maxSlidingWindow(nums, k));
+// console.log(maxSlidingWindow(nums, k)); // [10, 10, 9, 2]
