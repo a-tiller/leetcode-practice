6d35029915a85fac6deb7de33aed92d1da9855f5
Adam Tiller <a.j.tiller@gmail.com>
2020-07-31 07:02:32 -0700

    Regular N-th Fib.

--- JulyChallenge/31.ClimbStairs.js
+++ JulyChallenge/31.ClimbStairs.js
@@ -1,17 +1,20 @@
 var climbStairs = function(n) {
-  const steps = new Array(n + 2).fill(0);
-  steps.unshift(1)
+  let pp = 0;
+  let p = 0;
+  let c = 1;
+  let i = n;
 
-  const moves = [1, 2];
-
-  for (let i = 0; i < steps.length - 2; i++) {
-    moves.forEach(move => {
-      steps[i + move] += steps[i];
-    })
+  while (i > 0) {
+    pp = p;
+    p = c;
+    c = p + pp;
+    i--;
   }
-  return steps[n]
+
+  return c;
 };
 
-console.log(climbStairs(2)) // 2
-console.log(climbStairs(3)) // 3
-console.log(climbStairs(43)) // 701408733
+// console.log(climbStairs(1)) // 1
+// console.log(climbStairs(2)) // 2
+// console.log(climbStairs(3)) // 3
+// console.log(climbStairs(43)) // 701408733
