109a9b9ba887e8b646073d3549394b361fd78238
Adam Tiller <a.j.tiller@gmail.com>
2020-08-03 08:41:26 -0700

    O(1) space two pointers.

--- AugustChallenge/03.ValidPalindrome.js
+++ AugustChallenge/03.ValidPalindrome.js
@@ -1,13 +1,28 @@
 var isPalindrome = function(s) {
-  const sanitized = s.toLowerCase().split(/[^a-z0-9]/).join('');
 
-  let j = sanitized.length - 1;
+  const isAlphaNumeric = c => c.match(/[A-Za-z0-9]/);
 
-  for (let i = 0; i < j; i++) {
-    if (sanitized[i] !== sanitized[j]) {
-      return false;
+  let l = 0;
+  let r = s.length - 1;
+
+  while (l < r) {
+    if(!isAlphaNumeric(s[l])) {
+      l++;
+      continue;
     }
-    j--;
+
+    if(!isAlphaNumeric(s[r])) {
+      r--;
+      continue;
+    }
+
+    if (s[l] === s[r] || s[l].toLowerCase() === s[r].toLowerCase()) {
+      l++;
+      r--;
+      continue;
+    }
+
+    return false;
   }
 
   return true;
@@ -18,3 +33,13 @@ var isPalindrome = function(s) {
 
 // str = "race a car"
 // console.log(isPalindrome(str))
+
+// str = "0P"
+// console.log(isPalindrome(str))
+
+// str = "00"
+// console.log(isPalindrome(str))
+
+// str = ",."
+// console.log(isPalindrome(str))
+
