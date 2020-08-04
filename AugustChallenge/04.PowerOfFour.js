42e7edb319dd331d3a78a4680916de5c45f8a388
Adam Tiller <a.j.tiller@gmail.com>
2020-08-04 07:02:14 -0700

    Bitmasking solution.

--- AugustChallenge/04.PowerOfFour.js
+++ AugustChallenge/04.PowerOfFour.js
@@ -1,21 +1,16 @@
 var isPowerOfFour = function(num) {
-  if (num <= 0) return false
+  if (num & (num - 1)) return false   // checks to see if only a single bit is set (i.e. is a power of two)
+  if (num <= 0) return false // checks to see if non-negative (since powers of 4 are all positive)
 
-  let n = num;
-  let counter = 0
+  const MASK = 0xAAAAAAAA >> 1 // 0xA = 1010; 0xAA = 10101010; so 0xAA >> 1 = 01010101 (i.e. bits set in the indices corresponding to powers of 4)
 
-  while (n > 1) {
-   n >>= 2
-   counter++
-  }
-
-  return num === Math.pow(4, counter)
+  return !!(num & MASK); // 0 if the single bit is in a non-corresponding index, num if it's a power of four; double negation converts to boolean
 };
 
-console.log(isPowerOfFour(1))
-console.log(isPowerOfFour(2))
-console.log(isPowerOfFour(3))
-console.log(isPowerOfFour(4))
-console.log(isPowerOfFour(8))
-console.log(isPowerOfFour(16))
-console.log(isPowerOfFour(17))
+// console.log(isPowerOfFour(1)) // true
+// console.log(isPowerOfFour(2)) // false
+// console.log(isPowerOfFour(3)) // false
+// console.log(isPowerOfFour(4)) // true
+// console.log(isPowerOfFour(8)) // false
+// console.log(isPowerOfFour(16)) // true
+// console.log(isPowerOfFour(17)) // false
