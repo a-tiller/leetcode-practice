1f7f9f0b79c27380b5f29a77e4ddbba3c348bab6
Adam Tiller <a.j.tiller@gmail.com>
2020-08-05 11:37:56 -0700

    No Map/Set.

--- JulyChallenge/18.CourseScheduleII.js
+++ JulyChallenge/18.CourseScheduleII.js
@@ -1,43 +1,36 @@
 var findOrder = function(numCourses, prerequisites) {
-  const visited = new Set();
+  const visited = Array(numCourses).fill(0);
   const order = [];
+  let cyclic = false;
 
-  const adjacency = new Array(numCourses).fill(0).map(() => []);
+  const adjacency = visited.map(() => []);
   for (const edge of prerequisites) {
     [course, prereq] = edge;
     adjacency[course].push(prereq);
   }
 
-  const dfs = (node, visiting = new Set()) => {
-    if (visited.has(node)) {
-      return true;
-    }
+  const dfs = (node) => {
+    if (cyclic || visited[node]) return;
 
-    if (visiting.has(node)) {
-      return false
-    }
+    visited[node] = 1;
 
-    const p = adjacency[node];
-    visiting.add(node);
-
-    for (let i = 0; i < p.length; i++) {
-      if (!dfs(p[i], visiting)) {
-        return false;
+    const prereqs = adjacency[node];
+    for (let i = 0; i < prereqs.length; i++) {
+      const next = prereqs[i];
+      if (visited[next] === 1) {
+        cyclic = true;
+        return;
       }
+      dfs(next)
     }
 
-    visiting.delete(node);
-    visited.add(node);
+    visited[node] = 2;
     order.push(node);
-
-    return true;
   };
 
   for (let i = 0; i < numCourses; i++) {
-    if (!dfs(i)) {
-      return []
-    }
+    dfs(i)
   }
 
-  return order;
+  return cyclic ? [] : order;
 };
