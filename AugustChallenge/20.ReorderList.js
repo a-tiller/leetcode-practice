61c054e0468bdd38d98482307ec8e7715ce94042
Adam Tiller <a.j.tiller@gmail.com>
2020-08-20 08:33:14 -0700

    Constant space: split, reverse, merge.

--- AugustChallenge/20.ReorderList.js
+++ AugustChallenge/20.ReorderList.js
@@ -1,25 +1,32 @@
 var reorderList = function(head) {
-  if (!head || !head.next) return head;
+  // find midpoint and split the lists
+  let mid = head;
+  let head2 = head;
 
-  const store = [];
+  while (head2.next){
+    mid = mid.next;
+    head2.next && head2 = head2.next;
+    head2.next && head2 = head2.next;
+  }
+
+  head2 = mid.next;
+  mid.next = null;
+
+  // reverse second half of list
+  let prev = null;
+
+  while(head2) {
+    [head2.next, head2, prev] = [prev, head2.next, head2];
+  }
+
+  head2 = prev;
+
+  // merge lists
   let current = head;
 
-  while (current) {
-    store.push(current);
-    current = current.next;
+  while (head2) {
+    [current.next, head2.next, current, head2] = [head2, current.next, current.next, head2.next];
   }
-
-  let left = 0;
-  let right = store.length - 1;
-
-  while (left < right) {
-    store[right].next = store[left].next;
-    store[left].next = store[right];
-    left++;
-    right--;
-  }
-
-  store[left].next = null;
 
   return head;
 };
