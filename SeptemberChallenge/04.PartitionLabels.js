77930765f12cfd56bd0c1b7a74f8c86a9a464949
Adam Tiller <a.j.tiller@gmail.com>
2020-09-04 07:29:22 -0700

    Iterative.

--- SeptemberChallenge/04.PartitionLabels.js
+++ SeptemberChallenge/04.PartitionLabels.js
@@ -2,32 +2,24 @@ var partitionLabels = function(S) {
   if (S.length === 0) return [];
 
   const results = [];
-  const characterMap = new Map();
+  const lastOccurrence = {};
 
   for (let i = 0; i < S.length; i++) {
-    const char = S[i];
-    if (!characterMap.has(char)) {
-      characterMap.set(char, new Set());
+    lastOccurrence[S[i]] = i;
+  }
+
+  let start = 0;
+  let end = lastOccurrence[S[0]];
+
+  for(let i = 0; i < S.length; i++) {
+    end = Math.max(end, lastOccurrence[S[i]]);
+    if (i === end) {
+      results.push(i - start + 1);
+      start = i + 1;
     }
-    characterMap.get(char).add(i);
   }
 
-  let max = Math.max(...characterMap.get(S[0]));
-  let extend = true;
-
-  while (extend) {
-    extend = false;
-    const toCheck = new Set(S.slice(1,max));
-    toCheck.forEach((char) => {
-      const lastOccurrence = Math.max(...characterMap.get(char))
-      if (lastOccurrence > max) {
-        max = lastOccurrence;
-        extend = true;
-      }
-    });
-  }
-  results.push(max + 1);
-  return results.concat(partitionLabels(S.slice(max + 1)));
+  return results;
 };
 
 console.log(partitionLabels("ababcbacadefegdehijhklij"))
