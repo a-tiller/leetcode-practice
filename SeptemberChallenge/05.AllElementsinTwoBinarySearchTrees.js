ac26dd67599a2e5f5669041abbf08954504b25b1
Adam Tiller <a.j.tiller@gmail.com>
2020-09-05 13:05:40 -0700

    Parse to array, merge.

---
+++ SeptemberChallenge/05.AllElementsinTwoBinarySearchTrees.js
@@ -0,0 +1,35 @@
+var getAllElements = function(root1, root2) {
+  const arr1 = [];
+  const arr2 = [];
+  let results = [];
+
+  function inOrder(node, arr) {
+    if (!node) return;
+    inOrder(node.left, arr);
+    arr.push(node.val);
+    inOrder(node.right, arr);
+  }
+
+  inOrder(root1, arr1);
+  inOrder(root2, arr2);
+
+  while (arr1.length || arr2.length) {
+    if (!arr1.length) {
+      results = results.concat(arr2);
+      break;
+    }
+
+    if (!arr2.length) {
+      results = results.concat(arr1);
+      break;
+    }
+
+    if (arr1[0] > arr2[0]) {
+      results.push(arr2.shift());
+    } else {
+      results.push(arr1.shift());
+    }
+  }
+
+  return results;
+};
