7b4e6d01d512cdaed94f9556e2dc067419a7f176
Adam Tiller <a.j.tiller@gmail.com>
2020-09-06 09:22:36 -0700

    No negative offsents.

--- SeptemberChallenge/06.ImageOverlap.js
+++ SeptemberChallenge/06.ImageOverlap.js
@@ -1,24 +1,27 @@
 var largestOverlap = function(A, B) {
-  function compare(position, offset, fixed = A, shifted = B) {
+  function compare(position, offset, size, fixed, shifted) {
     const [pr, pc] = position;
     if (!fixed[pr][pc]) return false;
     const or = pr + offset[0];
     const oc = pc + offset[1];
-    if (or < 0 || or >= shifted.length || oc < 0 || oc >= shifted[0].length || !shifted[or][oc]) return false;
+    if (or < 0 || or >= size || oc < 0 || oc >= size || !shifted[or][oc]) return false;
     return true;
   }
 
   let result = 0;
+  let N = A.length;
 
-  for (let rOffset = 1 - B.length; rOffset < B.length; rOffset++) {
-    for (let cOffset = 1 - B[0].length; cOffset < B[0].length; cOffset++) {
-      let counter = 0;
-      for (let row = 0; row < A.length; row++) {
-        for (let col = 0; col < A[0].length; col++) {
-          if (compare([row, col], [rOffset, cOffset])) counter++;
+  for (let rOffset = 0; rOffset < N; rOffset++) {
+    for (let cOffset = 0; cOffset < N; cOffset++) {
+      let counterA = 0;
+      let counterB = 0;
+      for (let row = 0; row < N; row++) {
+        for (let col = 0; col < N; col++) {
+          if (compare([row, col], [rOffset, cOffset], N, A, B)) counterA++;
+          if (compare([row, col], [rOffset, cOffset], N, B, A)) counterB++;
         }
       }
-      result = Math.max(result, counter);
+      result = Math.max(result, counterA, counterB);
     }
   }
 
@@ -38,4 +41,4 @@ console.log(largestOverlap(testA, testB)); // 1
 testA = [[1,0],[1,0]]
 testB = [[0,1],[1,0]]
 
-console.log(largestOverlap(testA, testB));
+console.log(largestOverlap(testA, testB)); // 1
