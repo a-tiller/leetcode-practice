d0055f2669035d0c5092dca36c21f0902b4ce8af
Adam Tiller <a.j.tiller@gmail.com>
2020-09-10 07:07:55 -0700

    One map.

--- SeptemberChallenge/10.BullsAndCows.js
+++ SeptemberChallenge/10.BullsAndCows.js
@@ -2,41 +2,34 @@ var getHint = function(secret, guess) {
   let bulls = 0;
   let cows = 0;
 
-  const secretSpares = new Map();
-  const guessSpares = new Map();
+  const guessSpares = new Array(10).fill(0);
 
   for (let i = 0; i < guess.length; i++) {
-    if (secret[i] === guess[i]) {
-      bulls++;
+    if (guess[i] === secret[i]) {
+      bulls++
     } else {
-      if (!secretSpares.has(secret[i])) {
-        secretSpares.set(secret[i], 1);
-      } else {
-        secretSpares.set(secret[i], secretSpares.get(secret[i]) + 1);
-      }
-      if (!guessSpares.has(guess[i])) {
-        guessSpares.set(guess[i], 1);
-      } else {
-        guessSpares.set(guess[i], guessSpares.get(guess[i]) + 1);
-      }
+      guessSpares[guess[i]]++;
     }
   }
 
-  guessSpares.forEach((v, k) => {
-    cows += Math.min(v, (secretSpares.get(k) || 0));
-  })
+  for (let i = 0; i < secret.length; i++) {
+    if (guess[i] !== secret[i] && guessSpares[secret[i]]) {
+      cows++;
+      guessSpares[secret[i]]--;
+    }
+  }
 
   return `${bulls}A${cows}B`;
 };
 
 let s = ""
 let g = ""
-console.log(getHint(s, g));
+console.log(getHint(s, g));  // 0A0B
 
 s = "1807"
 g = "7810"
-console.log(getHint(s, g));
+console.log(getHint(s, g)); // 1A3B
 
 s = "1123"
 g = "0111"
-console.log(getHint(s, g));
+console.log(getHint(s, g)); // 1A1B
