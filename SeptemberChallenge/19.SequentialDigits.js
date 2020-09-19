706fe3c68e9f03cfdf5b3b55d686c73a71d3de61
Adam Tiller <a.j.tiller@gmail.com>
2020-09-19 11:49:59 -0700

    Sliding window on string.

--- SeptemberChallenge/19.SequentialDigits.js
+++ SeptemberChallenge/19.SequentialDigits.js
@@ -1,39 +1,15 @@
 var sequentialDigits = function(low, high) {
   const results = [];
+  const digits = '123456789';
 
-  function buildNum(s, l) {
-    let num = s;
-    let next = s + 1;
+  const min = low.toString().length;
+  const max = high.toString().length;
 
-    for (let i = 1; i < l; i++) {
-      num *= 10;
-      num += next;
-      next++;
+  for (let i = min; i <= max; i++) {
+    for (let j = 0; j <= digits.length - i; j++) {
+      num = +digits.substring(j, j + i);
+      if (num >= low && num <= high) results.push(num);
     }
-
-    return num;
-  }
-
-  const lowString = low.toString()
-  let len = lowString.length;
-
-  const candidate = +lowString[0];
-  let start = buildNum(candidate, len) >= low ? candidate : candidate + 1;
-
-  while (true) {
-    if (start > 10 - len) {
-      len++;
-      if (len === 10) break;
-      start = 1;
-      continue;
-    }
-
-    const num = buildNum(start, len)
-
-    if (num > high) break;
-
-    results.push(num);
-    start++;
   }
 
    return results;
@@ -42,5 +18,5 @@ var sequentialDigits = function(low, high) {
 console.log(sequentialDigits(100,300)); // [123, 234]
 console.log(sequentialDigits(1000,13000)); // [1234,2345,3456,4567,5678,6789,12345]
 console.log(sequentialDigits(0,200)); // [1,2,3,4,5,6,7,8,9,12,23,34,45,56,67,78,89,123]
-// console.log(sequentialDigits(10,1000000000));
+console.log(sequentialDigits(10,1000000000));
 console.log(sequentialDigits(342,353)) // [345]
