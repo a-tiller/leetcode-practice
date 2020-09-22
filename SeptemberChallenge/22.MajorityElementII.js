f2a45b48dd36edfd07fb956c422b1acc357af516
Adam Tiller <a.j.tiller@gmail.com>
2020-09-22 08:57:22 -0700

    O(n) time and O(1) space.

--- SeptemberChallenge/22.MajorityElementII.js
+++ SeptemberChallenge/22.MajorityElementII.js
@@ -1,17 +1,37 @@
 var majorityElement = function(nums) {
-  const results = [];
-  const limit = Math.min(nums.length / 3);
 
-  const counts = {};
+  let [candidateOne, candidateTwo, countOne, countTwo] = [null, null, 0, 0];
 
   nums.forEach(num => {
-    if (!counts.hasOwnProperty(num)) counts[num] = 0;
-    counts[num]++;
-  })
+    if (num === candidateOne) countOne++;
+    else if (num === candidateTwo) countTwo++;
+    else if (countOne === 0) {
+      candidateOne = num;
+      countOne++;
+    }
+    else if (countTwo === 0) {
+      candidateTwo = num;
+      countTwo++;
+    }
+    else {
+      countOne--;
+      countTwo--;
+    }
+  });
 
-  for (const num in counts) {
-    if (counts[num] > limit) results.push(num);
-  }
+  countOne = countTwo = 0;
+
+  nums.forEach(num => {
+    if (num === candidateOne) countOne++;
+    else if (num === candidateTwo) countTwo++;
+  });
+
+  const results = [];
+  const limit = Math.min(nums.length / 3);
+  if (countOne > limit) results.push(candidateOne);
+  if (countTwo > limit) results.push(candidateTwo);
 
   return results;
 };
+
+console.log(majorityElement([1,2]))
