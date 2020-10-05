9ab3af6d877ac71f1212c9f0b7213a601e463ed8
Adam Tiller <a.j.tiller@gmail.com>
2020-10-05 10:40:14 -0400

    One pass bitwise.

--- OctoberChallenge/05.ComplementOfBase10Integer.js
+++ OctoberChallenge/05.ComplementOfBase10Integer.js
@@ -1,17 +1,21 @@
 var bitwiseComplement = function(N) {
-  let maxSet = 0;
+  let pending = 0;
   let result = 0;
 
   for (let i = 0; i < 32; i++) {
-    if ((1 << i) & N) maxSet = i;
+    if ((1 << i) & N) {
+      result += pending;
+      pending = 0;
+    } else {
+      pending += 1 << i;
+    }
   }
 
-  for (let i = 0; i <= maxSet; i++) {
-    if (!((1 << i) & N)) result += 1 << i;
-  }
-
-  return result;
+  return N ? result : 1;
 };
-console.log(bitwiseComplement(5))
-console.log(bitwiseComplement(7))
-console.log(bitwiseComplement(10))
+
+console.log(bitwiseComplement(5)) // 2
+console.log(bitwiseComplement(7)) // 0
+console.log(bitwiseComplement(10)) // 5
+console.log(bitwiseComplement(0)) // 1
+
