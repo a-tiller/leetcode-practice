f28c3dcf6ca6d8de3546642b3662fa8a68a9bcc8
Adam Tiller <a.j.tiller@gmail.com>
2020-10-06 09:13:57 -0400

    iterative

--- OctoberChallenge/06.InsertIntoABinarySearchTree.js
+++ OctoberChallenge/06.InsertIntoABinarySearchTree.js
@@ -1,13 +1,20 @@
 var insertIntoBST = function(root, val) {
   if (!root) return new TreeNode(val, null, null);
 
-  if (val < root.val) {
-    if (root.left) insertIntoBST(root.left, val);
-    else root.left = new TreeNode(val, null, null)
-  } else if (val > root.val) {
-    if (root.right) insertIntoBST(root.right, val);
-    else root.right = new TreeNode(val, null, null)
+  let current = root;
+  while (true) {
+    if (val < current.val) {
+      if (current.left) current = current.left;
+      else {
+        current.left = new TreeNode(val, null, null);
+        return root;
+      }
+    } else {
+      if (current.right) current = current.right;
+      else {
+        current.right = new TreeNode(val, null, null);
+        return root;
+      }
+    }
   }
-
-  return root;
 };
