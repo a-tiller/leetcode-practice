030221242c1c1056c550eef19b28b7eb4d526da6
Adam Tiller <a.j.tiller@gmail.com>
2020-10-15 10:18:56 -0400

    Flipping.

--- OctoberChallenge/15.RotateArray.js
+++ OctoberChallenge/15.RotateArray.js
@@ -1,7 +1,15 @@
 var rotate = function(nums, k) {
   k = k % nums.length;
 
-  while (k--) {
-    nums.unshift(nums.pop());
+  flip(nums, 0, nums.length - 1);
+  flip(nums, 0, k - 1);
+  flip(nums, k, nums.length - 1);
+};
+
+const flip = (arr, start, end) => {
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
   }
 };
