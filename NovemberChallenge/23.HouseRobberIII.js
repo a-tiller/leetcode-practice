3f6b25abacfc5a0710d7d46db6c981312fe99b47
Adam Tiller <a.j.tiller@gmail.com>
2020-11-23 09:26:43 -0500

    Memoized recursion.

--- NovemberChallenge/23.HouseRobberIII.js
+++ NovemberChallenge/23.HouseRobberIII.js
@@ -1,17 +1,15 @@
 var rob = function(root) {
+  const dfs = (node) => {
+    if (node === null) return [0, 0];
 
-  const dfs = (node, canRob = true) => {
-    if (node === null) return 0;
+    const [leftWithRoot, leftWithoutRoot] = dfs(node.left)
+    const [rightWithRoot, rightWithoutRoot] = dfs(node.right)
 
-    if (!canRob) {
-      return dfs(node.left) + dfs(node.right);
-    }
+    const withRoot = node.val + leftWithoutRoot + rightWithoutRoot;
+    const withoutRoot = Math.max(leftWithRoot, leftWithoutRoot) + Math.max(rightWithRoot, rightWithoutRoot);
 
-    const robHere = node.val + dfs(node.left, !canRob) + dfs(node.right, !canRob);
-    const skipHere = dfs(node.left) + dfs(node.right);
-
-    return Math.max(robHere, skipHere);
+    return [withRoot, withoutRoot];
   }
 
-  return dfs(root);
+  return Math.max(...dfs(root));
 };
