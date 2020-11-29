a75e7528deb256a5d00dd2ad1a5f0f7acbb56fbd
Adam Tiller <a.j.tiller@gmail.com>
2020-11-29 08:13:38 -0500

    Recursion and mutation.

---
+++ NovemberChallenge/29.JumpGameIII.js
@@ -0,0 +1,8 @@
+var canReach = function(arr, start) {
+  if (start < 0 || start >= arr.length) return false;
+  if (arr[start] < 0) return false;
+  if (arr[start] === 0) return true;
+
+  arr[start] = -arr[start];
+  return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start])
+};
