eed1580cd994118e0a809c9b422f175a3aecf846
Adam Tiller <a.j.tiller@gmail.com>
2020-12-01 08:28:29 -0500

    Golf.

--- DecemberChallenge/01.MaximumDepthOfBinaryTree.js
+++ DecemberChallenge/01.MaximumDepthOfBinaryTree.js
@@ -1,16 +1,3 @@
 var maxDepth = function(root) {
-  let maxDepth = 0;
-
-  const dfs = (node, depth = 1) => {
-    if (!node) return;
-
-    maxDepth = Math.max(depth, maxDepth);
-
-    dfs(node.left, depth + 1);
-    dfs(node.right, depth + 1);
-  }
-
-  dfs(root);
-
-  return maxDepth;
+  return root ? 1 + (Math.max(maxDepth(root.left), maxDepth(root.right))) : 0;
 };
