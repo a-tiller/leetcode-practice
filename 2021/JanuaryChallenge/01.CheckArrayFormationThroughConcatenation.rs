2e4f84830c95d7229f101cc1eaaeaa3335310bb8
Adam Tiller <a.j.tiller@gmail.com>
2021-01-01 13:49:04 -0500

    Replace while with for.

--- 2021/JanuaryChallenge/01.CheckArrayFormationThroughConcatenation.rs
+++ 2021/JanuaryChallenge/01.CheckArrayFormationThroughConcatenation.rs
@@ -1,37 +1,32 @@
 use std::collections::HashMap;
 
 impl Solution {
-  pub fn can_form_array(arr: Vec<i32>, pieces: Vec<Vec<i32>>) -> bool {
-      // hash pieces by value at index 0
-      let mut pieces_index: HashMap<i32, &Vec<i32>> = HashMap::with_capacity(pieces.len());
+    pub fn can_form_array(arr: Vec<i32>, pieces: Vec<Vec<i32>>) -> bool {
 
-      for piece in pieces.iter() {
-        pieces_index.insert(piece[0], &piece);
-      }
+        let mut pieces_index: HashMap<i32, &Vec<i32>> = HashMap::with_capacity(pieces.len());
 
-      // walk the sequence
-      let mut position = 0;
+        for piece in pieces.iter() {
+          pieces_index.insert(piece[0], &piece);
+        }
 
-      while position < arr.len() {
-          match pieces_index.get(&arr[position]) {
-              None => {
-                  return false;
-              }
-              Some(subarray) => {
-                  let mut subarray_position = 0;
+        let mut position = 0;
 
-                  while subarray_position < subarray.len() {
-                      if (subarray[subarray_position] != arr[position]) {
-                        return false;
-                      }
+        while position < arr.len() {
+            match pieces_index.get(&arr[position]) {
+                None => {
+                    return false;
+                }
+                Some(piece) => {
+                    for number in piece.iter() {
+                        if (&arr[position] != number) {
+                            return false;
+                        }
+                        position += 1;
+                    }
+                }
+            }
+        }
 
-                      position += 1;
-                      subarray_position += 1;
-                  }
-              }
-          }
-      }
-
-      return true;
-  }
+        return true;
+    }
 }
