22bab57010d0092481b95af13be954d9164750d0
Adam Tiller <a.j.tiller@gmail.com>
2021-01-03 13:30:51 -0500

    Vector solution.

--- 2021/01.JanuaryChallenge/03.BeautifulArrangement.rs
+++ 2021/01.JanuaryChallenge/03.BeautifulArrangement.rs
@@ -1,25 +1,24 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn count_arrangement(n: i32) -> i32 {
-        let mut numbers: HashSet<i32> = HashSet::with_capacity(n as usize);
+        let mut numbers: Vec<i32> = Vec::with_capacity(n as usize);
         for number in 1..(n + 1) {
-          numbers.insert(number);
+          numbers.push(number);
         }
 
-        fn beautiful_arranger(mut remaining: &mut HashSet<i32>, size: &i32, mut count: i32) -> i32 {
+        fn beautiful_arranger(mut remaining: &mut Vec<i32>, size: &i32, mut count: i32) -> i32 {
           if remaining.is_empty() {
             return count + 1;
           }
 
           let position = 1 + size - remaining.len() as i32;
-          let todo = remaining.clone();
 
-          for number in todo.iter() {
+          for i in 0..remaining.len() {
+            let number = remaining[i];
+
             if number % position == 0 || position % number == 0 {
-              remaining.remove(&number);
+              remaining.remove(i);
               count = beautiful_arranger(&mut remaining, &size, count);
-              remaining.insert(*number);
+              remaining.insert(i, number);
             }
           }
 
