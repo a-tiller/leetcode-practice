776566ac58aaac5ab7b0dfaa4d16d6c90542f30c
Adam Tiller <a.j.tiller@gmail.com>
2021-01-03 14:02:35 -0500

    Safer vector access.

--- 2021/01.JanuaryChallenge/03.BeautifulArrangement.rs
+++ 2021/01.JanuaryChallenge/03.BeautifulArrangement.rs
@@ -1,30 +1,35 @@
 impl Solution {
-    pub fn count_arrangement(n: i32) -> i32 {
-        let mut numbers: Vec<i32> = Vec::with_capacity(n as usize);
-        for number in 1..(n + 1) {
-          numbers.push(number);
+  pub fn count_arrangement(n: i32) -> i32 {
+      let mut numbers: Vec<i32> = Vec::with_capacity(n as usize);
+      for number in 1..(n + 1) {
+        numbers.push(number);
+      }
+
+      fn beautiful_arranger(mut remaining: &mut Vec<i32>, size: &i32, mut count: i32) -> i32 {
+        if remaining.is_empty() {
+          return count + 1;
         }
 
-        fn beautiful_arranger(mut remaining: &mut Vec<i32>, size: &i32, mut count: i32) -> i32 {
-          if remaining.is_empty() {
-            return count + 1;
-          }
+        let position = 1 + size - remaining.len() as i32;
 
-          let position = 1 + size - remaining.len() as i32;
-
-          for i in 0..remaining.len() {
-            let number = remaining[i];
-
-            if number % position == 0 || position % number == 0 {
-              remaining.remove(i);
-              count = beautiful_arranger(&mut remaining, &size, count);
-              remaining.insert(i, number);
+        for i in 0..remaining.len() {
+          let number = match remaining.get(i) {
+            Some(i) => *i,
+            None => {
+              continue;
             }
-          }
+          };
 
-          count
+          if number % position == 0 || position % number == 0 {
+            remaining.remove(i);
+            count = beautiful_arranger(&mut remaining, &size, count);
+            remaining.insert(i, number);
+          }
         }
 
-        beautiful_arranger(&mut numbers, &n, 0)
-    }
+        count
+      }
+
+      beautiful_arranger(&mut numbers, &n, 0)
+  }
 }
