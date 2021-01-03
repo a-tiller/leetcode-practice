66e233cf468d7b80ea47a056e4a78b7b15c9dee0
Adam Tiller <a.j.tiller@gmail.com>
2021-01-03 16:35:02 -0500

    Abbreviate vector construction.

--- 2021/01.JanuaryChallenge/03.BeautifulArrangement.rs
+++ 2021/01.JanuaryChallenge/03.BeautifulArrangement.rs
@@ -1,35 +1,33 @@
 impl Solution {
-  pub fn count_arrangement(n: i32) -> i32 {
-      let mut numbers: Vec<i32> = Vec::with_capacity(n as usize);
-      for number in 1..(n + 1) {
-        numbers.push(number);
-      }
-
-      fn beautiful_arranger(mut remaining: &mut Vec<i32>, size: &i32, mut count: i32) -> i32 {
-        if remaining.is_empty() {
-          return count + 1;
-        }
-
-        let position = 1 + size - remaining.len() as i32;
-
-        for i in 0..remaining.len() {
-          let number = match remaining.get(i) {
-            Some(i) => *i,
-            None => {
-              continue;
+    pub fn count_arrangement(n: i32) -> i32 {
+        fn beautiful_arranger(mut remaining: &mut Vec<i32>, size: &i32, mut count: i32) -> i32 {
+            if remaining.is_empty() {
+                return count + 1;
             }
-          };
 
-          if number % position == 0 || position % number == 0 {
-            remaining.remove(i);
-            count = beautiful_arranger(&mut remaining, &size, count);
-            remaining.insert(i, number);
-          }
+            let position = 1 + size - remaining.len() as i32;
+
+            for i in 0..remaining.len() {
+                // let number = remaining[i];
+                let number = match remaining.get(i) {
+                    Some(value) => *value,
+                    None => {
+                        continue;
+                    }
+                };
+
+                if number % position == 0 || position % number == 0 {
+                    remaining.remove(i);
+                    count = beautiful_arranger(&mut remaining, &size, count);
+                    remaining.insert(i, number);
+                }
+            }
+
+            count
         }
 
-        count
-      }
+        let mut numbers: Vec<i32> = (1..n + 1).collect();
 
-      beautiful_arranger(&mut numbers, &n, 0)
-  }
+        beautiful_arranger(&mut numbers, &n, 0)
+    }
 }
